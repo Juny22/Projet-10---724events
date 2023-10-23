3bac4ee1fd039d99640f07e3a1090c8c54a2e31b
Lang Ya <lang.ya351@gmail.com>
2023-10-23 08:51:18 +0200

    modif Events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,38 +11,41 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
+
+  // Filtrer les événements en fonction de la catégorie sélectionnée
+  const filteredEvents = data?.events.filter((event) => {
+    if (!type || event.type === type) {
+      console.log(`Événement inclus : ${event.title}, Type : ${event.type}`);
       return true;
     }
+    console.log(`Événement exclu : ${event.title}, Type : ${event.type}`);
     return false;
-  });
+  }).slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+
+  // Calculez le nombre de pages nécessaires en fonction du nombre d'événements filtrés
+  const pageNumber = filteredEvents ? Math.ceil(filteredEvents.length / PER_PAGE) : 0;
+  
+  // Obtenez la liste des catégories uniques à partir des événements
+  const typeList = [...new Set(data?.events.map((event) => event.type))];
+
   return (
     <>
-      {error && <div>An error occured</div>}
+      {error && <div>An error occurred</div>}
       {data === null ? (
-        "loading"
+        "Loading"
       ) : (
         <>
           <h3 className="SelectTitle">Catégories</h3>
           <Select
-            selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            selection={typeList}
+            onChange={changeType}
           />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
@@ -60,12 +63,18 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
+          {Array.from({ length: pageNumber }).map((_, index) => {
+          const page = index + 1;
+          return (
+              <a
+                key={`page-${page}`} // Utilisation d'une clé unique basée sur le numéro de page
+                href="#events"
+                onClick={() => setCurrentPage(page)}
+              >
+                {page}
               </a>
-            ))}
+            );
+          })}
           </div>
         </>
       )}
