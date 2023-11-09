1fb694704c42c76584c24f37cce3f67f09da6ab5
Lang Ya <lang.ya351@gmail.com>
2023-11-09 11:14:11 +0100

    fix test

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -5,61 +5,43 @@ import "./style.scss";
 
 const EventCard = ({
   imageSrc,
-  imageAlt, 
-  date = new Date(),
+  imageAlt,
+  date,
   title,
   label,
-  small = false,
+  small,
   ...props
-}) => /* (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
+}) => (
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
     </div>
-  ); */
-  {
-    console.log("Valeur de date :", date); // Ajoutez cette ligne pour vérifier la valeur de la date
-  
-    return (
-      <div
-        data-testid="card-testid"
-        className={`EventCard${small ? " EventCard--small" : ""}`}
-        {...props}
-      >
-        <div className="EventCard__imageContainer">
-          <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-          <div className="EventCard__label">{label}</div>
-        </div>
-        <div className="EventCard__descriptionContainer">
-          <div className="EventCard__title">{title}</div>
-          <div className="EventCard__month">{getMonth(date)}</div>
-        </div>
-      </div>
-    );
-  };
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
   date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   small: PropTypes.bool,
   label: PropTypes.string.isRequired,
 };
 
 EventCard.defaultProps = {
+  imageSrc: "",
   imageAlt: "image",
+  title: "",
   small: false,
-}
+};
 
 export default EventCard;
